dedfc4fb3aa1045dd87e01c12626b76cb5019587
Unicorn9x <unicorn9x@yahoo.com.vn>
2025-03-06 20:32:00 +0700

    Add files via upload

--- plugin.rb
+++ plugin.rb
@@ -1,27 +1,47 @@
 # name: spoiler-alert-auth
 # about: Extends Discourse Spoiler-Alert to only allow logged-in users to reveal spoilers
-# version: 1.4.0
-# authors: Unicorn9x
+# version: 1.0.0
+# authors: Your Name
 
 enabled_site_setting :spoiler_auth_enabled
 
 register_asset "stylesheets/spoiler_auth.scss"
 
 after_initialize do
-  # Register our custom markdown processor
-  register_html_builder('post') do |context|
+  # Process spoilers in posts
+  on(:post_process_cooked) do |doc, post|
     begin
-      next if context.nil? || context.post.nil?
+      return if doc.nil? || post.nil?
       
       # Only process if the user is not logged in
-      if !context.user
-        context.post.cooked = context.post.cooked.gsub(
-          /<div class="spoiler">(.*?)<\/div>/m,
-          '<div class="spoiler spoiler-auth spoiler-blurred"><div class="spoiler-auth-prompt"><a href="/login" class="btn btn-primary">' + I18n.t("spoiler_auth.login_to_reveal") + '</a></div></div>'
-        )
+      if !post.user
+        doc.css(".spoiler").each do |el|
+          next if el.nil? || el.inner_html.blank?
+          
+          begin
+            # Store original content
+            el["data-original-content"] = el.inner_html
+            
+            # Replace content with login prompt
+            el.inner_html = <<~HTML
+              <div class="spoiler-auth-prompt">
+                <a href="/login" class="btn btn-primary">
+                  #{I18n.t("spoiler_auth.login_to_reveal")}
+                </a>
+              </div>
+            HTML
+            
+            # Add necessary classes
+            el.add_class("spoiler-auth")
+            el.add_class("spoiler-blurred")
+          rescue => e
+            Rails.logger.error("Spoiler Auth Plugin Error (post_process_cooked element): #{e.message}")
+            next
+          end
+        end
       end
     rescue => e
-      Rails.logger.error("Spoiler Auth Plugin Error (html_builder): #{e.message}\n#{e.backtrace.join("\n")}")
+      Rails.logger.error("Spoiler Auth Plugin Error (post_process_cooked): #{e.message}\n#{e.backtrace.join("\n")}")
     end
   end
 
