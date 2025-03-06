f2c006eb6568391c200474c09041810460ee3ed6
Unicorn9x <unicorn9x@yahoo.com.vn>
2025-03-06 22:25:32 +0700

    Add files via upload

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: spoiler-alert-auth
 # about: Extends Discourse Spoiler-Alert to only allow logged-in users to reveal spoilers
-# version: 1.6.0
+# version: 1.7.0
 # authors: Unicorn9x
 
 enabled_site_setting :spoiler_auth_enabled
