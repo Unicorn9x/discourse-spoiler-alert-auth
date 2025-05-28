7bd84d05adadf6aaf1282ce049bbcc42e9b3745c
Unicorn9x <unicorn9x@yahoo.com.vn>
2025-05-28 14:03:49 +0700

    update 1.1.0

---
+++ ember-cli-build.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { build } = require('@embroider/compat');
+
+module.exports = function (defaults) {
+  return build(defaults, {
+    staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
+    staticHelpers: true,
+    staticComponents: true,
+    skipBabel: [
+      {
+        package: 'qunit'
+      }
+    ]
+  });
+}; 
