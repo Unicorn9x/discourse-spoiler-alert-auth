552d9e3b437925ac444ac0effc611e6503677641
Unicorn9x <unicorn9x@yahoo.com.vn>
2025-05-28 13:29:34 +0700

    update 1.0.8

---
+++ config/initializers/load.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module ::SpoilerAuthAlert
+  class Engine < ::Rails::Engine
+    engine_name "spoiler_auth_alert"
+    isolate_namespace SpoilerAuthAlert
+  end
+end 
